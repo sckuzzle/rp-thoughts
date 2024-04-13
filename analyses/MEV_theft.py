4854a84f69e55a1c45a3f4591b58eefe17c9367c
unknown <sckuzzle@gmail.com>
2024-04-13 19:13:46 -0400

    Removed old code, added comments

--- analyses/MEV_theft.py
+++ analyses/MEV_theft.py
@@ -1,24 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Apr 13 15:17:18 2024
-
-@author: Cruncher
+Script for calculating how profitable MEV theft is at various bond curves and comparing it to honest APR.
 """
-import unittest
+
 import math
-import pdb
+
 
 import plotly.graph_objects as go
 import numpy as np
-
-assertAlmostEqual = unittest.case.TestCase.assertAlmostEqual
-
 
 #globals
 solo_apr = 0.05
 
 
 def plot_data(x_data, y_data:dict, drag_line = None, title = None, x_title = 'Node ETH', y_title = 'Profitability (APR)', renderer = 'png'):
+  """Plotting function using plotly."""
   fig = go.Figure()
   for legend, y in y_data.items():
     fig.add_trace(go.Scatter(x=x_data, y=np.array(y),
@@ -42,48 +38,9 @@ def plot_data(x_data, y_data:dict, drag_line = None, title = None, x_title = 'No
 max_penalty = lambda node_eth, commission: node_eth/ (1-commission)  #max you can penalize the node for
 unsmoothing_eq = lambda max_penalty: 0.23*math.exp(-0.69*math.log(max_penalty))  #eth at risk per validator per year
 
-# def profitability_curve(nETH_per_pool: list, commission_per_pool: list) -> list:
-#   """Calculates the total APR for honest nodes at each minipool point."""
-#   assert len(nETH_per_pool) == len(commission_per_pool), 'invalid list lengths!'
-  
-#   total_profit = 0
-#   total_nETH = 0
-#   apr = []
-  
-#   for i in range(len(nETH_per_pool)):
-#     total_profit += solo_apr * (commission_per_pool[i] * (32-nETH_per_pool[i]) + nETH_per_pool[i])
-#     total_nETH += nETH_per_pool[i]
-#     total_apr = total_profit / total_nETH
-    
-#     apr.append(total_apr)
-    
-#   return apr
-
-# def theft_curve(nETH_per_pool: list, commission_per_pool: list):
-#   assert len(nETH_per_pool) == len(commission_per_pool), 'invalid list lengths!'
-  
-#   commission = 0
-#   pETH = 0
-#   nETH = 0
-#   apr = []
-  
-#   for i in range(len(nETH_per_pool)):
-    
-#     pETH_in_new_pool = 32 - nETH_per_pool[i]
-#     commission = ( pETH * commission + pETH_in_new_pool * commission_per_pool[i] ) / (pETH_in_new_pool + pETH)
-    
-#     pETH += pETH_in_new_pool
-#     nETH += nETH_per_pool[i]
-
-#     max_penalizable = max_penalty(nETH, commission)
-#     unsmoothing_fee = unsmoothing_eq(max_penalizable)
-#     total_possible_theft = unsmoothing_fee * i
-    
-#     apr.append(total_possible_theft / nETH)
-    
-#   return apr
 
 def generate_curves(nETH_per_pool: list, commission_per_pool: list):
+  """Steps through each number of minipools, updating total nETH / pETH and calculating APRs at each point, before returning lists."""
   
   honest_total_profit = 0
   nETH = 0
@@ -131,6 +88,7 @@ def generate_curves(nETH_per_pool: list, commission_per_pool: list):
   
 
 def simple_example():
+  """Looks at MEV theft and honest APR.  Does not calculate drag."""
   satellite_eth = 1.5
   initial_eth = [4, 4]
   pools_to_graph = 100
@@ -160,17 +118,16 @@ def example_with_drag():
   satellite_eth = 1.5
   
   #samus
-  # initial_eth = [8, 0, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 0.5, 0]
+  # initial_eth = [4, 4, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 0.5, 0]
   
-  #Default 
+  #Default.  ETH bond required per minipool will follow this list, then continue at satellite_eth for additional minipools
   initial_eth = [4, 4]
   pools_to_graph = 50
   NO_commission = 0.025
-  protocol_cut = 0.14 - NO_commission
+  protocol_cut = 0.14 - NO_commission #Used for calculated how much drag there is on the protocol
   
+  # Construct lists of nETH and commission for each minipool
   nETH_per_pool = [*initial_eth, *[satellite_eth for n in range(pools_to_graph - len(initial_eth))]]
-  
-  # nETH_per_pool = [*initial_eth, *[initial_eth[-1]+satellite_eth*(1+n) for n in range(pools_to_graph - len(initial_eth))]]
   commission_per_pool = [NO_commission for n in range(pools_to_graph)]
   
   # Calculate total nETH up to that point
@@ -180,7 +137,10 @@ def example_with_drag():
     nETH += nETH_per_pool[n]
     total_nETH.append(nETH)
   
+  #Create curves
   honest_apr, theft_apr, total_dishonest_apr, protocol_apr, honest_protocol_apr = generate_curves(nETH_per_pool, commission_per_pool)
+  
+  #Find where it is most profitable to steal MEV and then calculate what their APR is
   max_theft_index = total_dishonest_apr.index(max(total_dishonest_apr))
   max_theft_apr = total_dishonest_apr[max_theft_index]
   drag_lower = honest_apr[max_theft_index]
@@ -196,14 +156,8 @@ def example_with_drag():
             )
   
   print(f'rETH Drag: {rETH_drag*100}% or Protocol Drag:{protocol_drag*100}%')
+  print(f'Max theft occurs at {max_theft_index+1} validators and {total_nETH[max_theft_index]} nETH')
   
-def protocol_value(honest_apr, nETH_per_pool):
-  
-  
-  total_revenue = 0
-  for i in range(len(nETH_per_pool)):
-    pass
-    
   
   
 
